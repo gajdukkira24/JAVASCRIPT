803a4be2910891fc5e58a8e711f5c277bd2e46da
Kira <gajdukkira232@gmail.com>
2025-01-26 11:29:25 +0200

    upadate

--- js/vars-tipes-1lessons.js
+++ js/vars-tipes-1lessons.js
@@ -1,6 +1,28 @@
+
 const myname = 'Kira'
  console.log(myname);
 const product ='bananas'
 console.log(product)
 const pricePerItmes = '10грн'
 console.log( pricePerItmes)
+
+
+
+const speed =120
+console.log(speed)
+const distance=617.54
+console.log(distance)
+const isAdmin = true;
+const congration = "Привіт вітаю на нашому сайті"
+alert(congration)
+
+const myName = "Яке твоє ім'я"
+const userName =prompt(myName)
+console.log(userName)
+console.log( typeof userName)
+
+const shouldrennu =confirm("Чи хочете продовжити підписку");
+console.log(shouldrennu  )
+
+
+
